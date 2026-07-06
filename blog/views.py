831d334f8c8d3923b82e26781e9b417ff880848e
douglasmg7 <douglasmg7@gmail.com>
2026-07-06 11:56:00 -0300

    html for blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
+from django.urls import reverse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+def posts(request):
+    pass
+
+def post_detail(request, slug):
+    pass
+#  def monthly_challenge(request, month):
+    #  try:
+        #  return render(request, 'challenges/challenge.html', {'month': month, 'message': monthly_challenges[month]})
+    #  except:
+        #  raise Http404()
+        #  #  return HttpResponseNotFound('Month not supported yet')
+
