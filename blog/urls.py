9f33213ee6ce62bc9f4306360c116fd3c16bd521
douglasmg7 <douglasmg7@gmail.com>
2026-07-07 17:24:23 -0300

    css and url links

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='blog_index'),
-    path('posts', views.posts, name='all_posts'),
+    path('', views.index, name='blog-index'),
+    path('posts', views.posts, name='all-posts'),
     path('posts/<slug:slug>', views.post_detail, name='post-detial'),
 ]
